4f598d4fd09e95732a09dfa787ce5b9ba0dce209
Mihail <mihail.stevcev@gmail.com>
2017-09-22 14:48:40 +0200

    Add DS_Store to .gitignore

---
+++ Top Contributers/Top Contributers/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Top Contributers
+//
+//  Created by Mihail Stevcev on 9/22/17.
+//  Copyright © 2017 Rumberos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Top Contributers/Top Contributers/ViewControllers/Contributors/Feed/ViewController/ContributorsFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContributorsFeedViewController.h
+//  Top Contributers
+//
+//  Created by Mihail Stevcev on 9/22/17.
+//  Copyright © 2017 Rumberos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContributorsFeedViewController : UIViewController
+
+@end
